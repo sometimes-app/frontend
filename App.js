164831f9673cdd08a3bcec5081783d8fb2cfd2b7
Antonio <ascala@Antonios-MacBook-Pro-2.local>
2023-01-21 20:25:21 -0500

    startup component

--- App.js
+++ App.js
@@ -1,21 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import Startup from './src/Startup'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Startup>
+    </Startup>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
