f1c3fe4fb3ed496f3bc4895c5e5eaf0f322b9bbd
Antonio <ascala@Antonios-MacBook-Pro-2.local>
2023-02-12 17:49:52 -0500

    complete fade in animation component

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import AnimatedText from './AnimatedText';
+import FadeInAnimatedText from './FadeInAnimatedText';
 
 const LoginScreen = ( {navigation} ) => {
     return (
       <View style={styles.container}>
-        <AnimatedText text={'Sometimes'}></AnimatedText>
+        <FadeInAnimatedText 
+          text={'Sometimes'} 
+          byChar={true} 
+          duration={1000}
+          textSize={32}
+        >
+        </FadeInAnimatedText>
 
         <Text>Login Screen</Text>
         <Button
