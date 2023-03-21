f6fb3aed365e1c067dae1cfb31e9ebee77300b70
nathan7432 <steelersfan5052@yahoo.com>
2023-03-21 13:39:00 -0400

    styled resgister screen

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -3,6 +3,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
 import { createUserWithEmailAndPassword } from "firebase/auth"
 import { authentication } from '../../firebaseConfig'
+import { colors, globalStyle } from '../styles/styles';
+import Header from '../components/Header';
 
 /** Register Screen */
 const RegisterScreen = ( {navigation} ) => {
@@ -18,21 +20,19 @@ const RegisterScreen = ( {navigation} ) => {
     }
 
     return (
-      <View style={styles.container}>
-        <Text>Register Screen</Text>
-            <View>
-                <TextInput placeholder='Email' value={email} onChangeText={setEmail}/>
-                <TextInput placeholder='Password' value={password} secureTextEntry={true} onChangeText={setPassword}/>
-                <TouchableOpacity onPress={()=>{RegisterUser()}}>
-                    <Text>Sign Up</Text>
-                </TouchableOpacity>
+      <View style={globalStyle.background}>
+        <View style={globalStyle.container}>
+          <Header showBack={true} navigation={navigation} showProfile={false}/>
+            <View style={styles.container}>
+              <Text style={styles.createText}>Create an account</Text>
+              <TextInput style={styles.email} placeholder='Email' value={email} onChangeText={setEmail} placeholderTextColor='gray'/>
+              <TextInput style={styles.password} placeholder='Password' value={password} secureTextEntry={true} onChangeText={setPassword} placeholderTextColor='gray'/>
+              <TouchableOpacity style={styles.signUp} onPress={()=>{RegisterUser()}}>
+                <Text style={{ fontWeight: 'bold' }}>Sign Up</Text>
+              </TouchableOpacity>
             </View>
-            < StatusBar style='auto'/>
-        <Button
-          title="Back"
-          onPress={() => {navigation.navigate('Login')}}
-        />
-        <StatusBar style="auto" />
+          <StatusBar style="light" />
+        </View>
       </View>
     )
   }
@@ -40,9 +40,42 @@ const RegisterScreen = ( {navigation} ) => {
   const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      // maxHeight: '50%',
+      // backgroundColor: 'navy',
+    },
+    createText: {
+      color: colors.accentColor,
+      fontSize: 30,
+      fontWeight: 'bold',
+      marginHorizontal: '5%',
+      marginTop: '5%',
+    },
+    email: {
+      color: colors.accentColor,
+      fontSize: 18,
+      marginHorizontal: '5%',
+      marginTop: '10%',
+      paddingBottom: '2%',
+      borderBottomColor: 'rgba(255,255,255,.25)',
+      borderBottomWidth: 1,
+    },
+    password: {
+      color: colors.accentColor,
+      fontSize: 18,
+      marginHorizontal: '5%',
+      marginTop: '10%',
+      paddingBottom: '2%',
+      borderBottomColor: 'rgba(255,255,255,.25)',
+      borderBottomWidth: 1,
+    },
+    signUp: {
+      display: 'flex',
       alignItems: 'center',
-      justifyContent: 'center',
+      marginTop: '10%',
+      marginHorizontal: '5%',
+      backgroundColor: colors.primaryColor,
+      borderRadius: 25,
+      padding: '4%'
     },
   });
 
