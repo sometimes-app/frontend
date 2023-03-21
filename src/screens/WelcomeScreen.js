b4c7c6183c2513c1c865223c2b1aae1cb5331204
nathan7432 <steelersfan5052@yahoo.com>
2023-03-21 13:43:47 -0400

    add global styling welcome screen

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,36 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import FadeInAnimatedText from '../components/FadeInAnimatedText';
+import { globalStyle } from '../styles/styles';
 
 /** Screen to pick a profile or create one. */
 const WelcomeScreen = ( {navigation} ) => {
     return (
-      <View style={styles.container}>
-        <FadeInAnimatedText 
-          text={'Sometimes'} 
-          byChar={false} 
-          duration={1000}
-          textSize={32}
-        >
-        </FadeInAnimatedText>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={() => {navigation.navigate('Register')}}>
-            <Text>Create Profile</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => {navigation.navigate('Login')}}>
-            <Text>Login</Text>
-          </TouchableOpacity>
+      <View style={globalStyle.background}>
+        <View style={globalStyle.container}>
+          <FadeInAnimatedText 
+            text={'Sometimes'} 
+            byChar={false} 
+            duration={1000}
+            textSize={32}
+            >
+          </FadeInAnimatedText>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={() => {navigation.navigate('Register')}}>
+              <Text>Create Profile</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => {navigation.navigate('Login')}}>
+              <Text>Login</Text>
+            </TouchableOpacity>
+          </View>
+          <StatusBar style='light' />
         </View>
-        <StatusBar style='light' />
       </View>
     )
   }
 
   const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: 'black',
-    },
     buttonContainer: {
       flexDirection: 'row',
       justifyContent: 'space-evenly',
