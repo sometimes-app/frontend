5aee56ff0600610d24e898c673cc237b6bcf331a
Antonio <ascala@Antonios-MacBook-Pro-2.local>
2023-03-23 16:16:15 -0400

    lint for jest

--- Tests/App.test.js
+++ Tests/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 
-import App from './App'
+import App from '../App'
 // jest.useFakeTimers() this doesn't work
 
 describe('<App />', () => {
