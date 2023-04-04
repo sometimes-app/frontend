ffbdee897175b0ca0812f8d7fa066f63298d2ecc
nathan7432 <steelersfan5052@yahoo.com>
2023-04-03 21:49:23 -0400

    profile tests

---
+++ Tests/screens/ProfileScreen.test.js
@@ -0,0 +1,16 @@
+import { describe, beforeEach, it, expect } from '@jest/globals'
+import { render, screen, fireEvent } from '@testing-library/react-native'
+import ProfileScreen from '../../src/screens/ProfileScreen'
+import { mockNavigation } from '../mocks/navigation-mock'
+import { signOut } from 'firebase/auth'
+
+describe('Profile', () => {
+  beforeEach(() => {
+    render(<ProfileScreen navigation={mockNavigation} />)
+  })
+
+  it('should sign out', () => {
+    fireEvent.press(screen.getByText('Sign Out'))
+    expect(signOut).toBeCalledTimes(1)
+  })
+})
